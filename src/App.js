5eaaa5bf02c58ce316e4870c837ddbd66fa4e5f1
Deckason <chibuikemchidera@gmail.com>
2022-08-16 04:21:17 +0100

    first temperature control app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [temp, setTemp] = useState(10)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div style={{background: temp > 15 ? "#fc8301" : "#14148d"}} className="degrees">
+          <p>{temp}<sup>o</sup>C</p>
+        </div>
+        <div className="buttons">
+          <button onClick={()=>{setTemp(temp === 0 ? 0 : temp-1)}}>-</button>
+          <button onClick={()=>{setTemp(temp === 30 ? 30 : temp+1)}}>+</button>
+        </div>
+      </div>
     </div>
   );
 }
